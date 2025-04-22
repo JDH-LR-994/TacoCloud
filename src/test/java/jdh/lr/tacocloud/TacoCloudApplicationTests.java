64bf58aec137753a712c98a28afe782af4e6a341
JDH-LR-994 <jdh994lr@gmail.com>
2025-04-22 22:46:05 +0300

    Chapter 1

---
+++ src/test/java/jdh/lr/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package jdh.lr.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
